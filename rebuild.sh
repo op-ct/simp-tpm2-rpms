3358cd6a552b205e9c388a14c8ef1ed75b6b9a4d
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-04-16 16:45:51 +0000

    Fix simp-tpm2-abrmd SELinux policy

--- rebuild.sh
+++ rebuild.sh
@@ -25,6 +25,8 @@ SYSTEMD
 sudo cp "${TMPFILE}" /etc/systemd/system/tpm2-abrmd.service.d/override.conf
 sudo chmod 0644 /etc/systemd/system/tpm2-abrmd.service.d/override.conf
 sudo systemctl daemon-reload
+sudo restorecon /usr/local/sbin/tpm2-abrmd
 sudo systemctl enable tpm2-abrmd.service
 sudo systemctl start tpm2-abrmd.service
+sudo udevadm control --reload-rules && sudo udevadm trigger
 ) |& tee /vagrant/_rebuild.log
